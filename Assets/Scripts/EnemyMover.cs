2c0d1d3ec8b90a327f77a667fa79a6e7a87ef903
Alex Miranda <am27237@gmail.com>
2021-10-19 13:42:32 +0800

    Signed-off-by: Alex Miranda <am27237@gmail.com> Implemented Couroutine and Waypoints

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoints> path = new List<Waypoints>();
+    [SerializeField] float waitSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintWaypointName());
+    }
+
+    IEnumerator PrintWaypointName()
+    {
+        foreach (Waypoints waypoints in path)
+        {
+            transform.position = waypoints.transform.position;
+            yield return new WaitForSeconds(waitSpeed);
+        }
+    }
+}
