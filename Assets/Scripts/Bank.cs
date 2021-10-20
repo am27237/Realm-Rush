d970d98adfd24cbd47cbe53a5c4ac063b5e0b442
Alex Miranda <am27237@gmail.com>
2021-10-20 14:33:59 +0800

    Signed-off-by: Alex Miranda <am27237@gmail.com> Implemented Score UI

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,31 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
     [SerializeField] int currentbalance;
     public int CurrentBalance { get { return currentbalance; } }
+    [SerializeField] TextMeshProUGUI displayBalance;
 
     private void Awake()
     {
         currentbalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentbalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentbalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if (currentbalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    private void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentbalance;
     }
 
     void ReloadScene()
