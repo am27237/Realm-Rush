976ec2297ea3d28b59a977133f92cbd8fbf52a95
Alex Miranda <am27237@gmail.com>
2021-10-23 23:30:06 +0800

    Signed-off-by: Alex Miranda <am27237@gmail.com> Implemented tower timer

--- Assets/Tower/Towers.cs
+++ Assets/Tower/Towers.cs
@@ -6,6 +6,13 @@ public class Towers : MonoBehaviour
 {
 
     [SerializeField] int cost = 75;
+    [SerializeField] float buildDelay = 1f;
+
+    void Start()
+    {
+        StartCoroutine(Build());
+    }
+
 
     public bool CreateTower(Towers towers, Vector3 position)
     {
@@ -24,5 +31,27 @@ public class Towers : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
