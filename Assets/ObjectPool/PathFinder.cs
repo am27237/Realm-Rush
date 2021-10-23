b83fe8c5880fb8ea70a40b6af3708c99bfa49035
Alex Miranda <am27237@gmail.com>
2021-10-23 11:56:46 +0800

    Signed-off-by: Alex Miranda <am27237@gmail.com> Implemented exploring the world

--- Assets/ObjectPool/PathFinder.cs
+++ Assets/ObjectPool/PathFinder.cs
@@ -1,13 +1,20 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PathFinder : MonoBehaviour
 {
-    [SerializeField] Node currentSearchNode;
+    [SerializeField] Vector2Int startCoordinates;
+    [SerializeField] Vector2Int destinationCoordinates;
+    Node startNode;
+    Node destinationNode;
+    Node currentSearchNode;
+    Queue<Node> frontier = new Queue<Node>();
+    Dictionary<Vector2Int, Node> reached = new Dictionary<Vector2Int, Node>();
+
     Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
     GridManager gridManager;
-    Dictionary<Vector2Int, Node> grid;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
     private void Awake()
     {
@@ -16,11 +23,34 @@ public class PathFinder : MonoBehaviour
         {
             grid = gridManager.Grid;
         }
+
+        startNode = new Node(startCoordinates, true);
+        destinationNode = new Node(destinationCoordinates, true);
     }
 
     private void Start()
     {
-        ExploreNeighbors();
+        //ExploreNeighbors();
+        BreadthFirstSearch();
+    }
+
+    void BreadthFirstSearch()
+    {
+        bool isRunning = true;
+        frontier.Enqueue(startNode);
+        reached.Add(startCoordinates, startNode);
+
+        while ( frontier.Count > 0 && isRunning )
+        {
+            currentSearchNode = frontier.Dequeue();
+            currentSearchNode.isExplored = true;
+            ExploreNeighbors();
+            if (currentSearchNode.coordinates == destinationCoordinates)
+            {
+                isRunning = false;
+            }
+        }
+
     }
 
     void ExploreNeighbors()
@@ -34,9 +64,18 @@ public class PathFinder : MonoBehaviour
                 neighbors.Add(grid[neighborCoords]);
 
                 //To remove after testing
-                grid[neighborCoords].isExplored = true;
-                grid[currentSearchNode.coordinates].isPath = true;
+                //grid[neighborCoords].isExplored = true;
+                //grid[currentSearchNode.coordinates].isPath = true;
             }
+        }
+
+        foreach (Node neighbor in neighbors)
+        {
+            if (!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable)
+            {
+                reached.Add(neighbor.coordinates, neighbor);
+                frontier.Enqueue(neighbor);
+            }    
         }
 
     }
